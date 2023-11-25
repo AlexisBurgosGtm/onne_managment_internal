f64ba630c136b23158d9b4de3098e6163d25d7f5
Alexis Burgos (gtm) <34293632+AlexisBurgosGtm@users.noreply.github.com>
2023-11-24 20:57:24 -0600

    a

---
+++ build/controllers/CustomerVars.js
@@ -0,0 +1,12 @@
+let GlobalToken = 'VICEFARMA';
+let GlobalCodSucursal = '';
+
+let GlobalEmpnit = 'VICEFARMA000';
+let GlobalEmpNombre = 'VICEFARMA 1';
+
+
+let GlobalVentaMinima = 1;
+
+let dataEmpresas = [ 
+    {codsucursal:"VICEFARMA000",nomsucursal:"VICEFARMA 1",color:"success"}
+];
